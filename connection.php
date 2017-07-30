5c52917c437744c4c4ac20f184c671b7b41911e8
thesilentman1 <onurkurtic@gmail.com>
2017-07-29 19:49:39 -0700

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+
+$link = mysqli_connect(" ", " ", " ", " ");
+
+        if (mysqli_connect_error()) {
+            
+            die ("Database Connection Error");
+            
+        }
+
+?>
